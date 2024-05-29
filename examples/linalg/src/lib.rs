8340ce8b77c4e5c4812dc7115acb8b413234017b
Jacobgarm <jacobgarm@hotmail.dk>
2024-05-29 16:31:01 +0200

    Half a macro

--- examples/linalg/src/lib.rs
+++ examples/linalg/src/lib.rs
@@ -5,6 +5,17 @@ use matrix::*;
 
 initiate_protocol!();
 
+macro_rules! unary {
+    ($name: tt, $content: expr) => {
+        #[wasm_func]
+        pub fn $name(arg: &[u8]) -> Vec<u8> {
+            let mat = Matrix::from_bytes(arg);
+            let res = $content;
+            res.to_bytes()
+        }
+    };
+}
+
 #[wasm_func]
 pub fn add(arg1: &[u8], arg2: &[u8]) -> Vec<u8> {
     let res = Matrix::from_bytes(arg1) + Matrix::from_bytes(arg2);
@@ -16,4 +27,6 @@ pub fn mul(arg1: &[u8], arg2: &[u8]) -> Vec<u8> {
     let res = Matrix::from_bytes(arg1) * Matrix::from_bytes(arg2);
     res.to_bytes()
 }
+
+//unary!(neg, { -mat });
 
