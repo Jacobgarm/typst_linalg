75f28ffbc5c99fde1228f57b056fe38637c51507
z4ppatron <z4ppatron@gmail.com>
2024-05-31 13:10:03 +0200

    Row reduced echelon form

--- pkg/main.typ
+++ pkg/main.typ
@@ -2,10 +2,8 @@
 #{
   let m1 = math.mat((20,20),(0,20))
   let m2 = math.mat((0.2,1),(5,7))
-  let m3 = math.mat((1, 4, 2), (0, 0, 5), (0, 2, 0))
-  mat.add(mat.REF(m1),m1)
-  $m1 m2 =$ 
-  mat.rowswap(m1,0,1)
+  let m3 = math.mat((0, 4, 2), (0, 0, 5), (1, 0, 0))
+  mat.RREF(m3)
 }
 
 
--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ mod vector;
 use matrix::*;
 fn main() {
     let m = Matrix {
-        rows: vec![vec![1.0, 2.0, 3.0], vec![4.0, 5.0, 6.0]],
+        rows: vec![vec![4.0, 5.0, 3.0, 0.0], vec![0.0, 0.0, 6.0, 1.0], vec![0.0, 0.0, 0.0, 7.0]]
     };
-    let (a, _) = m.REF();
+    let a = m.RREF();
     //let s = m.det();
     dbg!(a);
 }
